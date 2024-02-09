0a6f16823fd4cb49cc6a116042934ab537ac1c85
ccristil <ccristil@byu.edu>
2024-02-08 20:54:59 -0700

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $("#calculateButton").click(function (e) {
+        e.preventDefault(); // Prevent default form submission behavior
+        var hours = $("#hours").val();
+        if (hours <= 0) {
+            let message = "You must enter a number greater than 0.";
+            alert(message);
+        } else {
+            var rate = 15;
+            var total = hours * rate;
+            $("#output").val('$' + total);
+        }
+    });
+});
